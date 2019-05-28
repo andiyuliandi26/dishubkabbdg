2e57e5d8bdc2dd633ab5fb39640f13b3261bb560
Andi <andi.sugiarto@len.co.id>
2019-05-28 09:25:46 +0700

    remove again

--- backup/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=localhost;dbname=dishubka_cctv',
-	'username' => 'dishubka_cctv',
-	'password' => 'dishubkabbdg2017',
-	'charset' => 'utf8',
-];
