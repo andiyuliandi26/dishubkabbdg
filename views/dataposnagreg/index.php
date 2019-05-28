6019a643e9fcf5e2d3518e4595685786dd934328
Andi <andi.sugiarto@len.co.id>
2019-05-28 13:36:38 +0700

    perbaikan

--- views/dataposnagreg/index.php
+++ views/dataposnagreg/index.php
@@ -2,18 +2,6 @@
 
 use app\models\CCTV;
 use yii\helpers\Html;
-use dosamigos\google\maps\LatLng;
-use dosamigos\google\maps\services\DirectionsWayPoint;
-use dosamigos\google\maps\services\TravelMode;
-use dosamigos\google\maps\overlays\PolylineOptions;
-use dosamigos\google\maps\services\DirectionsRenderer;
-use dosamigos\google\maps\services\DirectionsService;
-use dosamigos\google\maps\overlays\InfoWindow;
-use dosamigos\google\maps\overlays\Marker;
-use dosamigos\google\maps\Map;
-use dosamigos\google\maps\services\DirectionsRequest;
-use dosamigos\google\maps\overlays\Polygon;
-use dosamigos\google\maps\layers\BicyclingLayer;
 
 $this->title='Dishub Kab Bandung';
 
