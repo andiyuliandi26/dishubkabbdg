6e75cbd2c705add92f4fb061da0a529bc030d095
Andi <andi.sugiarto@len.co.id>
2019-05-28 09:52:11 +0700

    test

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=localhost;dbname=andiyuliandi26_cctv',
+	'dsn' => 'mysql:host=localhosts;dbname=andiyuliandi26_cctv',
 	'username' => 'andiyuliandi26_cctv',
 	'password' => 'Dishub2018',
 	'charset' => 'utf8',
